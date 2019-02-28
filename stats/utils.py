14894b777053384367c1cc431e3efb9fc97e6f9f
HajimeKawanishi <hjm.kwns854@gmail.com>
2019-02-28 14:35:35 +0900

    sold_atでフィルタされたレコードを取得する部分をを関数に分離

--- stats/utils.py
+++ stats/utils.py
@@ -1,7 +1,24 @@
 from fruitsales.models import Sale
 
 
-def cumulate_sales_fruitwise(start_date, end_date):
+def get_sales_filtered_sold_at(start_datetime, end_datetime):
+    """Saleテーブルから一定期間内のレコードを取得
+
+    Args:
+        start_date: 期間の始まり(inclusive)
+        end_date: 期間の終わり(exclusive)
+    Returns:
+        一定期間内のレコードのリスト
+    """
+    sales = Sale.objects.all()
+
+    sales_filtered = [sale for sale in sales
+                    if (sale.sold_at >= start_datetime and sale.sold_at < end_datetime)]
+
+    return sales_filtered
+
+
+def cumulate_sales_fruitwise(start_datetime, end_datetime):
     """Saleテーブルから果物ごとに一定期間内の累計売上・個数を計算
 
     Args:
@@ -10,8 +27,9 @@ def cumulate_sales_fruitwise(start_date, end_date):
     Returns:
         期間内の売り上げ累計, 果物ごとの集計値
     """
-    sales = Sale.objects.filter(sold_at__gte=start_date,
-                        sold_at__lt=end_date)
+    # sales = Sale.objects.filter(sold_at__gte=start_datetime,
+    #                     sold_at__lt=end_datetime)
+    sales = filter_sales_sold_at(start_datetime, end_datetime)
     sales_amount = sum([sale.amount for sale in sales])
 
     # 果物ごとに集計
